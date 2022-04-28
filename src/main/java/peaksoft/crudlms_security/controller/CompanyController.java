31b3d08752ce5ed7cda186e7833bd555380b7759
Locadmin <ulukmyrza92@gmail.com>
2022-04-28 18:25:02 +0300

    Company html edit

--- src/main/java/peaksoft/crudlms_security/controller/CompanyController.java
+++ src/main/java/peaksoft/crudlms_security/controller/CompanyController.java
@@ -3,7 +3,9 @@ package peaksoft.crudlms_security.controller;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import peaksoft.crudlms_security.model.Company;
 import peaksoft.crudlms_security.service.CompanyService;
 
@@ -24,6 +26,18 @@ public class CompanyController {
         model.addAttribute("companyList",companyList);
 
         return "company/company_page";
+    }
+
+    @RequestMapping("/new")
+    public String createNewCompany(Model model){
+        Company company = new Company();
+        model.addAttribute("company", company);
+        return "company/new_company";
+    }
+    @RequestMapping(value = "/save",method = RequestMethod.POST)
+    public String save(@ModelAttribute("company")Company company){
+        service.save(company);
+        return "redirect:/company/";
     }
 
 }
